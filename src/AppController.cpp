e951ae3afd08fe3bf1bbe70e10debc043b37590d
Matt Gingold <m@gingold.com.au>
2014-04-12 13:31:20 -0700

    Changed media path for gameover laptop

--- src/AppController.cpp
+++ src/AppController.cpp
@@ -64,9 +64,9 @@ void AppController::setup(){
     appModel->setProperty("Ortho", true);
     
     //appModel->setProperty("MediaPath", (string)"/Users/gameoverlf/Desktop/LAF/media");
-    //appModel->setProperty("MediaPath", (string)"/Users/gameover/Desktop/LOTE/media");
+    appModel->setProperty("MediaPath", (string)"/Users/gameover/Desktop/LOTE/media");
     //appModel->setProperty("MediaPath", (string)"/Volumes/LongingAndForgetting02/LOTE/TESTRENDERS/mediaANIME");
-    appModel->setProperty("MediaPath", (string)"/Users/omid/Desktop/LAF/media");
+    //appModel->setProperty("MediaPath", (string)"/Users/omid/Desktop/LAF/media");
     appModel->setProperty("NumberPlayers", 10);
     appModel->setProperty("RectTrail", 200);
     
